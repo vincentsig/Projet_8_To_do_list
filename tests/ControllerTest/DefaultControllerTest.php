aa2adb054ecedd9fda3d3ece408a04d35d1280ef
vincentsig <47405610+vincentsig@users.noreply.github.com>
2020-11-29 21:22:11 -0500

    CI with functional test on SelectorTextContains

--- tests/ControllerTest/DefaultControllerTest.php
+++ tests/ControllerTest/DefaultControllerTest.php
@@ -16,5 +16,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !");
     }
 }
