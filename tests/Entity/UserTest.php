d52b7a8d76a3bec6ffad942207e649e9be7244b1
vincentsig <47405610+vincentsig@users.noreply.github.com>
2021-02-08 21:10:33 -0500

    test: update test for User entity

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -2,7 +2,6 @@
 
 namespace App\Tests\Entity;
 
-use App\Entity\Task;
 use App\Entity\User;
 use App\Tests\UserFactory;
 use DateTime;
@@ -11,35 +10,6 @@ use PHPUnit\Framework\TestCase;
 class UserTest extends TestCase
 {
     use UserFactory;
-
-    /**
-     * @test
-     */
-    public function add_task()
-    {
-        $user = new User();
-        $task = new Task();
-        $user->addTask($task);
-
-        $this->assertContains($task, $user->getTasks());
-    }
-
-    /**
-     * @test
-     */
-    public function remove_task()
-    {
-        $user = new User();
-        $task1 = new Task();
-        $task2 = new Task();
-
-        $user->addTask($task1);
-        $user->addTask($task2);
-        $this->assertContains($task2, $user->getTasks());
-
-        $user->removeTask($task2);
-        $this->assertNotContains($task2, $user->getTasks());
-    }
 
     /**
      * @test
